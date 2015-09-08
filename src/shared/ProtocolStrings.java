ab8ac7ee1582572487365105e9c7d19d104e8aa7
Bo Vilstrup Mortensen <bo.vilstrup@gmail.com>
2015-09-08 11:36:09 +0200

    new protocol Call added

--- src/shared/ProtocolStrings.java
+++ src/shared/ProtocolStrings.java
@@ -1,9 +1,35 @@
+//package shared;
+//
+///**
+// * @author Lars Mortensen
+// */
+//public class ProtocolStrings {
+//
+//    public static final String STOP = "STOP#";
+//}
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package shared;
 
 /**
- * @author Lars Mortensen
+ *
+ * @author bo
  */
 public class ProtocolStrings {
 
-    public static final String STOP = "STOP#";
-}
+    public static final String USER = "USER";
+    public static final String MSG = "MSG";
+    public static final String STOP = "STOP";
+    public static final String USERLIST = "USERLIST";
+    
+    public static final String SPLITTER = "#";
+    public static final String SEPARATOR = ",";
+
+    private ProtocolStrings() {}
+
+} // End of protocal Class
